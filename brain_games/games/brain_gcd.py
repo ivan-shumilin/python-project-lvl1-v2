ffe5cf693e5077e8e96db31ea270d8117e592f0b
ivan-shumiln <shumilin.i.v@gmail.com>
2021-09-18 18:38:33 +0300

    add brain-gcd

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,23 @@
+import random
+
+
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+
+def gcd_rem_division(num1, num2):
+    while num1 != 0 and num2 != 0:
+        if num1 >= num2:
+            num1 %= num2
+        else:
+            num2 %= num1
+    return num1 or num2
+
+def generate_round():
+    """Function defines conditions of the brain-calc game."""
+
+    first_random_number = random.randint(1, 10)
+    second_random_number = random.randint(1, 10)
+    question = '{0} {1}'.format(first_random_number, second_random_number)
+    correct_answer = str(gcd_rem_division(first_random_number,
+                                   second_random_number))
+    return question, correct_answer
