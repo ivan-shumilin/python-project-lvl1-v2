4070a46ea87a01861d39f034b1ab2e74dfdac2e9
ivan-shumiln <shumilin.i.v@gmail.com>
2021-09-18 18:14:50 +0300

    add brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from brain_games import engine
+from brain_games.games import brain_even
+
+
+def main():
+    engine.run(brain_even)
+
+
+if __name__ == '__main__':
+    main()
